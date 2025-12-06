acd4d467d746525ede5d42cf6df8f2848a3f9972
pupsinok <snastabi@gmail.com>
2025-12-06 16:39:27 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from "path"
 
 export default defineConfig({
   base: '/tripmate/', // <<< ОБЯЗАТЕЛЬНО! название репозитория в нижнем регистре
+    resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   plugins: [react()],
 })
-
